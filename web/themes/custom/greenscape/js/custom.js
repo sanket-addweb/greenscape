408e755163a5832b1a1d2553d7bd2ab459b660e9
sanket-addweb <sanket@addwebsolution.in>
2024-05-24 08:37:00 +0000

    Added slider section and resolved js issue.

--- web/themes/custom/greenscape/js/custom.js
+++ web/themes/custom/greenscape/js/custom.js
@@ -1,320 +1,293 @@
-/*---------------------------------------------------------------------
-    File Name: custom.js
----------------------------------------------------------------------*/
+(function ($, Drupal, once) {
+  'use strict';
 
-$(function () {
-	
-	"use strict";
-	
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	setTimeout(function () {
-		$('.loader_bg').fadeToggle();
-	}, 1500);
-	
-	/* JQuery Menu
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+  Drupal.behaviors.customJsBehavior = {
+    attach: function (context, settings) {
+      // Preloader
+      once('loaderBg', '.loader_bg', context).forEach(function (element) {
+        setTimeout(function () {
+          $(element).fadeToggle();
+        }, 1500);
+      });
 
-	$(document).ready(function () {
-		$('header nav').meanmenu();
-	});
-	
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	/* sticky
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".sticky-wrapper-header").sticky({topSpacing:0});
-	});
-	
-	/* Mouseover
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
-			}
-		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
-			$("#wrapper").removeClass('overlay');
-		});
-	});
-	
-	/* NiceScroll
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(".brand-box").niceScroll({
-		cursorcolor:"#9b9b9c",
-	});	
-	
-	/* NiceSelect
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-		$('select').niceSelect();
-	});	
-		
-	
-	/* Scroll to Top
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(window).on('scroll', function (){
-        scroll = $(window).scrollTop();
-        if (scroll >= 100){
-          $("#back-to-top").addClass('b-show_scrollBut')
-        }else{
-          $("#back-to-top").removeClass('b-show_scrollBut')
+      // jQuery Menu
+      once('meanmenu', 'header nav', context).forEach(function (element) {
+        $(element).meanmenu();
+      });
+
+      // Tooltip
+      once('tooltip', '[data-toggle="tooltip"]', context).forEach(function (element) {
+        $(element).tooltip();
+      });
+
+      // Sticky
+      once('sticky', '.sticky-wrapper-header', context).forEach(function (element) {
+        $(element).sticky({ topSpacing: 0 });
+      });
+
+      // Mouseover for megamenu
+      once('megamenu', '.main-menu ul li.megamenu', context).forEach(function (element) {
+        $(element).hover(
+          function () {
+            if (!$('#wrapper').hasClass('overlay')) {
+              $('#wrapper').addClass('overlay');
+            }
+          },
+          function () {
+            $('#wrapper').removeClass('overlay');
+          }
+        );
+      });
+
+      // NiceScroll
+      once('niceScroll', '.brand-box', context).forEach(function (element) {
+        $(element).niceScroll({
+          cursorcolor: "#9b9b9c",
+        });
+      });
+
+      // NiceSelect
+      once('niceSelect', 'select', context).forEach(function (element) {
+        $(element).niceSelect();
+      });
+
+      // Scroll to Top
+      once('scrollToTop', window).forEach(function () {
+        $(window).on('scroll', function () {
+          if ($(window).scrollTop() >= 100) {
+            $("#back-to-top").addClass('b-show_scrollBut');
+          } else {
+            $("#back-to-top").removeClass('b-show_scrollBut');
+          }
+        });
+      });
+
+      once('backToTop', '#back-to-top', context).forEach(function (element) {
+        $(element).on('click', function () {
+          $('body, html').animate({ scrollTop: 0 }, 1000);
+        });
+      });
+
+      // Contact form toggle
+      once('showMap', '#showMap', context).forEach(function (element) {
+        $(element).on('click', function (e) {
+          e.preventDefault();
+          $(".map_form_container").addClass("map_show");
+          $(".contact_heading").text("Location");
+        });
+      });
+
+      once('showForm', '#showForm', context).forEach(function (element) {
+        $(element).on('click', function (e) {
+          e.preventDefault();
+          $(".map_form_container").removeClass("map_show");
+          $(".contact_heading").text("Request A Call Back");
+        });
+      });
+
+      // Form validation
+      $.validator.setDefaults({
+        submitHandler: function () {
+          alert("submitted!");
         }
       });
-      $("#back-to-top").on("click", function(){
-        $('body,html').animate({
-          scrollTop: 0
-        }, 1000);
-    });
-	
-	/* Contact-form
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-  if (document.querySelector("#showMap")) { 
-	  	document.querySelector("#showMap").addEventListener("click", function (e) { 
-	  		e.preventDefault(); 
-	  		$(".map_form_container").addClass("map_show"); 
-	  		document.querySelector(".contact_heading").innerText = "Location"; 
-	  	}); 
-  	}
-	if (document.querySelector("#showForm")) { 
-		document.querySelector("#showForm").addEventListener("click", function (e) { 
-			e.preventDefault(); $(".map_form_container").removeClass("map_show"); 
-			document.querySelector(".contact_heading").innerText = "Request A Call Back"; 
-		}); 
-	}
 
-	$.validator.setDefaults( {
-		submitHandler: function () {
-			alert( "submitted!" );
-		}
-	} );
-	
-	$( document ).ready( function () {
-		$( "#contact-form" ).validate( {
-			rules: {
-				firstname: "required",
-				email: {
-					required: true,
-					email: true
-				},
-				lastname: "required",
-				message: "required",
-				agree: "required"
-			},
-			messages: {
-				firstname: "Please enter your firstname",
-				email: "Please enter a valid email address",
-				lastname: "Please enter your lastname",
-				username: {
-					required: "Please enter a username",
-					minlength: "Your username must consist of at least 2 characters"
-				},
-				message: "Please enter your Message",
-				agree: "Please accept our policy"
-			},
-			errorElement: "div",
-			errorPlacement: function ( error, element ) {
-				// Add the `help-block` class to the error element
-				error.addClass( "help-block" );
+      once('contactForm', '#contact-form', context).forEach(function (element) {
+        $(element).validate({
+          rules: {
+            firstname: "required",
+            email: {
+              required: true,
+              email: true
+            },
+            lastname: "required",
+            message: "required",
+            agree: "required"
+          },
+          messages: {
+            firstname: "Please enter your firstname",
+            email: "Please enter a valid email address",
+            lastname: "Please enter your lastname",
+            username: {
+              required: "Please enter a username",
+              minlength: "Your username must consist of at least 2 characters"
+            },
+            message: "Please enter your Message",
+            agree: "Please accept our policy"
+          },
+          errorElement: "div",
+          errorPlacement: function (error, element) {
+            error.addClass("help-block");
+            if (element.prop("type") === "checkbox") {
+              error.insertAfter(element.parent("input"));
+            } else {
+              error.insertAfter(element);
+            }
+          },
+          highlight: function (element, errorClass, validClass) {
+            $(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
+          },
+          unhighlight: function (element, errorClass, validClass) {
+            $(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+          }
+        });
+      });
 
-				if ( element.prop( "type" ) === "checkbox" ) {
-					error.insertAfter( element.parent( "input" ) );
-				} else {
-					error.insertAfter( element );
-				}
-			},
-			highlight: function ( element, errorClass, validClass ) {
-				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-error" ).removeClass( "has-success" );
-			},
-			unhighlight: function (element, errorClass, validClass) {
-				$( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-success" ).removeClass( "has-error" );
-			}
-		} );
-	});
-	
-	/* heroslider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	var swiper = new Swiper('.heroslider', {
-		spaceBetween: 30,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		paginationClickable: true,
-		loop: true,
-		autoplay: {
-			delay: 2500,
-			disableOnInteraction: false,
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			dynamicBullets: true
-		},
-	});
-	
+      // Swiper sliders
+      new Swiper('.heroslider', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true
+        }
+      });
 
-	/* Product Filters
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+      new Swiper('.swiper-product-filters', {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 30,
+        breakpoints: {
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            slidesPerColumn: 1,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            slidesPerColumn: 1,
+          },
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            slidesPerColumn: 1,
+          }
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true
+        }
+      });
 
-	var swiper = new Swiper('.swiper-product-filters', {
-		slidesPerView: 3,
-		slidesPerColumn: 2,
-		spaceBetween: 30,
-		breakpoints: {
-			1024: {
-			  slidesPerView: 3,
-			  spaceBetween: 30,
-			},
-			768: {
-			  slidesPerView: 2,
-			  spaceBetween: 30,
-			  slidesPerColumn: 1,
-			},
-			640: {
-			  slidesPerView: 2,
-			  spaceBetween: 20,
-			  slidesPerColumn: 1,
-			},
-			480: {
-			  slidesPerView: 1,
-			  spaceBetween: 10,
-			  slidesPerColumn: 1,
-			}
-		  },
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-			dynamicBullets: true
-		}
-    });
+      // Countdown
+      once('countdown', '[data-countdown]', context).forEach(function (element) {
+        var finalDate = $(element).data('countdown');
+        $(element).countdown(finalDate, function (event) {
+          $(element).html(event.strftime(''
+            + '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div>'
+            + '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div>'
+            + '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div>'
+            + '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div>'
+            + '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'
+          ));
+        });
+      });
 
-	/* Countdown
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$('[data-countdown]').each(function () {
-        var $this = $(this),
-		finalDate = $(this).data('countdown');
-		$this.countdown(finalDate, function (event) {
-			var $this = $(this).html(event.strftime(''
-			+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
-			+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
-		});
-    });
-	
-	/* Deal Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$('.deal-slider').slick({
-        dots: false,
-        infinite: false,
-		prevArrow: '.previous-deal',
-		nextArrow: '.next-deal',
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-		infinite: false,
-        responsive: [{
+      // Deal slider
+      once('dealSlider', '.deal-slider', context).forEach(function (element) {
+        $(element).slick({
+          dots: false,
+          infinite: false,
+          prevArrow: '.previous-deal',
+          nextArrow: '.next-deal',
+          speed: 500,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: false
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: false
             }
-        }, {
+          }, {
             breakpoint: 768,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+              slidesToShow: 2,
+              slidesToScroll: 2
             }
-        }, {
+          }, {
             breakpoint: 480,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
-        }]
-    });
-	
-	/* News Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+          }]
+        });
+      });
 
-
-
-	
-	$('#news-slider').slick({
-        dots: false,
-        infinite: false,
-		prevArrow: '.previous',
-		nextArrow: '.next',
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [{
+      // News slider
+      once('newsSlider', '#news-slider', context).forEach(function (element) {
+        $(element).slick({
+          dots: false,
+          infinite: false,
+          prevArrow: '.previous',
+          nextArrow: '.next',
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false
             }
-        }, {
+          }, {
             breakpoint: 600,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
-        }, {
+          }, {
             breakpoint: 480,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
-        }]
-    });
-	
-	/* Fancybox
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(".fancybox").fancybox({
-		maxWidth: 1200,
-		maxHeight: 600,
-		width: '70%',
-		height: '70%',
-	});
-	
-	/* Toggle sidebar
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
+          }]
+        });
+      });
+
+      // Fancybox
+      once('fancybox', '.fancybox', context).forEach(function (element) {
+        $(element).fancybox({
+          maxWidth: 1200,
+          maxHeight: 600,
+          width: '70%',
+          height: '70%',
+        });
+      });
+
+      // Sidebar collapse
+      once('sidebarCollapse', '#sidebarCollapse', context).forEach(function (element) {
+        $(element).on('click', function () {
           $('#sidebar').toggleClass('active');
           $(this).toggleClass('active');
-       });
-     });
+        });
+      });
 
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     // optional
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
-
-
-});
+      // Blog carousel
+      once('blogCarousel', '#blogCarousel', context).forEach(function (element) {
+        $(element).carousel({
+          interval: 5000
+        });
+      });
+    }
+  };
+})(jQuery, Drupal, once);
--- web/themes/custom/greenscape/js/owl.carousel.js
+++ web/themes/custom/greenscape/js/owl.carousel.js
@@ -1,23 +1,31 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:35,
-  nav:true,
-  center: true,
-  responsive:{
-    0:{
-        items:1,
-        margin:0
-    },
-    575:{
-      items:1,
-      margin:0
-    },
-    768:{
-      items:3,
-      margin:0
-    },
-    1000:{
-      items:3
+(function ($, Drupal, once) {
+  Drupal.behaviors.owlCarouselBehavior = {
+    attach: function (context, settings) {
+      once('owlCarouselBehavior', '.owl-carousel', context).forEach(function (element) {
+        $(element).owlCarousel({
+          loop: true,
+          margin: 35,
+          nav: true,
+          center: true,
+          responsive: {
+            0: {
+              items: 1,
+              margin: 0
+            },
+            575: {
+              items: 1,
+              margin: 0
+            },
+            768: {
+              items: 3,
+              margin: 0
+            },
+            1000: {
+              items: 3
+            }
+          }
+        });
+      });
     }
-  }
-})
+  };
+})(jQuery, Drupal, once);
